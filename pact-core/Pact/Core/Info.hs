854422dc8e91f1ffc0c125f03e55b742a6562402
Robert Soeldner <r.soeldner@gmail.com>
2023-03-20 14:44:26 +0100

    fix combineSpan

--- pact-core/Pact/Core/Info.hs
+++ pact-core/Pact/Core/Info.hs
@@ -19,5 +19,5 @@ instance Default SpanInfo where
 -- | Combine two Span infos
 -- and spit out how far down the expression spans.
 combineSpan :: SpanInfo -> SpanInfo -> SpanInfo
-combineSpan (SpanInfo l1 c1 _ _) (SpanInfo l2 _ _ _) =
-  SpanInfo l1 c1 (l2 - l1 + 1) 1
+combineSpan (SpanInfo l1 c1 _ _) (SpanInfo _ _ l2 c2) =
+  SpanInfo l1 c1 l2 c2
