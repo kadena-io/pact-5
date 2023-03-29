c673200514fdaf5b48c6d584fd73358bdb8f0fd1
Robert Soeldner <r.soeldner@gmail.com>
2023-03-29 10:12:25 +0200

    wip

--- pact-core-lsp/src/Pact/Core/LSP/Handlers.hs
+++ pact-core-lsp/src/Pact/Core/LSP/Handlers.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Pact.Core.LSP.Handlers where
 
@@ -17,18 +18,21 @@ import qualified Language.LSP.VFS as VFS
 import Data.Text.Utf16.Rope (toText)
 import Data.Text.Encoding (encodeUtf8)
 
-
 import qualified Pact.Core.Errors as P
 import Control.Lens.Getter (view)
 import qualified Pact.Core.Info as P
-import Pact.Core.Builtin (RawBuiltin, CoreBuiltin)
+import Pact.Core.Builtin (ReplRawBuiltin, BuiltinForm (..))
 import qualified Data.Map.Strict as M
-import Control.Monad.Except (MonadError(..), runExceptT)
-import qualified Data.Text as T
-import Pact.Core.Typed.Term (TopLevel(..))
-import qualified Pact.Core.Syntax.Lisp.Lexer as Lisp
-import Pact.Core.LSP.AnalyzeSource (analyzeSource, AnalyzeError (..), runAnalyze, AnalyzeState (..), AnalyzeResult (..))
+import Control.Monad.Except (MonadError(..))
+import Pact.Core.Typed.Term (OverloadedReplTopLevel, ReplTopLevel (..),
+                             Term (..), DefConst (..), Defun (..), Module (..), Def (..), termInfo)
+import Pact.Core.LSP.AnalyzeSource (analyzeSource, AnalyzeError (..), runAnalyze,
+                                    AnalyzeState (..), AnalyzeResult (..))
 import Control.Lens.Operators
+import Pact.Core.Names (NamedDeBruijn)
+import Control.Applicative ((<|>))
+import Data.Monoid (Alt (..))
+import qualified Data.Text as T
 
 liftLsp :: LspT ServerConfig IO a -> HandlerM a
 liftLsp = HandlerM . lift . lift
@@ -63,16 +67,19 @@ handleError :: (HandlerError -> HandlerM a) -> HandlerM a -> HandlerM a
 handleError = flip catchError
 
 hoverRequestHandler :: Handlers HandlerM
-hoverRequestHandler = requestHandler STextDocumentHover $ \req resp -> handleError (const (pure ())) $ do
+hoverRequestHandler = requestHandler STextDocumentHover $ \req resp ->
+  handleError (const (pure ())) $ do
   let
     _uri = req^.params.textDocument.uri
     pos  = req^.params.position
 
-  -- cache <- use ssCache
-  -- case M.lookup _uri cache of
-  --   Nothing -> throwError (NotCached _uri)
-  --   Just _ -> pure ()
-  pure ()
+  cache <- use ssCache
+  term <- case M.lookup _uri cache of
+    Nothing -> throwError (NotCached _uri)
+    Just (m, _) -> pure $ getAlt (foldMap (Alt . termAt pos) m)
+  let _contents = HoverContents (MarkupContent MkPlainText (T.pack (show (term,pos))))
+      _range = Nothing
+  resp (Right (Just Hover{..}))
 
 documentDiagnostics ::  Uri -> HandlerM ()
 documentDiagnostics _uri = do
@@ -116,3 +123,55 @@ pactErrorToDiagnostic pe = Diagnostic{..}
     _message = P.renderPactError pe
     _tags = Nothing
     _relatedInformation = Nothing
+
+
+termAt
+  :: Position
+  -> OverloadedReplTopLevel NamedDeBruijn ReplRawBuiltin P.SpanInfo
+  -> Maybe (OverloadedReplTopLevel NamedDeBruijn ReplRawBuiltin P.SpanInfo)
+termAt p = \case
+  RTLModule m -> goModule m
+  RTLInterface _inf ->  Nothing
+  RTLDefun df -> goDefun df
+  RTLDefConst dc -> goDefConst dc
+  RTLTerm term -> RTLTerm <$> goTerm term
+  where
+    goTerm term = if p `inside` view termInfo term
+                  then case term of
+                         t@(Lam _ b _) -> goTerm b <|> Just t
+                         t@(App tm1 tm2 _) ->
+                           goTerm tm1 <|> getAlt (foldMap (Alt . goTerm) tm2) <|> Just t
+                         t@(Let _ tm1 tm2 _) -> goTerm tm1 <|> goTerm tm2 <|> Just t
+                         t@(TyApp tm _ _) -> goTerm tm <|> Just t
+                         t@(TyAbs _ tm _) -> goTerm tm <|> Just t
+                         t@(Sequence tm1 tm2 _) -> goTerm tm1 <|> goTerm tm2 <|> Just t
+                         t@(Conditional op _) ->
+                           (case op of
+                               CAnd a b  -> goTerm a <|> goTerm b
+                               COr a b   -> goTerm a <|> goTerm b
+                               CIf a b c -> goTerm a <|> goTerm b <|> goTerm c) <|> Just t
+                         t@(ListLit _ tms _) -> getAlt (foldMap (Alt . goTerm) tms) <|> Just t
+                         t@(DynInvoke tm _ _) -> goTerm tm <|> Just t
+                         t@(Try tm1 tm2 _) -> goTerm tm1 <|> goTerm tm2 <|> Just t
+                         t -> Just t
+                  else Nothing
+    goDefun = \case
+      t@(Defun _ _ tm i)
+        | p `inside` i -> (RTLTerm <$> goTerm tm) <|> Just (RTLDefun t)
+      _otherwise -> Nothing
+    goDefConst = \case
+      t@(DefConst _ _ tm i)
+        | p `inside` i -> (RTLTerm <$> goTerm tm) <|> Just (RTLDefConst t)
+      _otherwise -> Nothing
+    goModule (Module _ defs _ _ _ _) = let
+      goDef = \case
+        Dfun t -> goDefun t
+        DConst t -> goDefConst t
+      in getAlt (foldMap (Alt . goDef) defs)
+
+-- | Check if a `Position` is contained within a `Span`
+inside :: Position -> P.SpanInfo -> Bool
+inside pos (P.SpanInfo sl sc el ec) = sPos <= pos && pos < ePos
+  where
+    sPos = Position (fromIntegral sl) (fromIntegral sc)
+    ePos = Position (fromIntegral el) (fromIntegral ec)
