040b245e755feb5e679c21176741486451fab8ca
John Wiegley <johnw@newartisans.com>
2023-04-27 12:35:07 -0700

    Add a flake.nix file

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Kadena's Pact smart contract language";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=4d2b37a84fad1091b9de401eb450aae66f1a741e";
+    # nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem
+      [ "x86_64-linux" "x86_64-darwin"
+        "aarch64-linux" "aarch64-darwin" ] (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system overlays;
+        inherit (haskellNix) config;
+      };
+      flake = pkgs.pact-core.flake {
+        # crossPlatforms = p: [ p.ghcjs ];
+      };
+      overlays = [ haskellNix.overlay
+        (final: prev: {
+          pact-core =
+            final.haskell-nix.project' {
+              src = ./.;
+              compiler-nix-name = "ghc8107";
+              shell.tools = {
+                cabal = {};
+                # hlint = {};
+              };
+              shell.buildInputs = with pkgs; [
+                zlib
+                z3
+                pkgconfig
+              ];
+              # shell.crossPlatforms = p: [ p.ghcjs ];
+            };
+        })
+      ];
+    in flake // {
+      packages.default = flake.packages."pact-core:exe:repl";
+    });
+}
