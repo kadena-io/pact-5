76e6d243a0c2db6f9f6056f3d085cb29f206bd1b
0xd34df00d <0xd34df00d@gmail.com>
2023-10-17 13:02:54 -0500

    Copy the Principal type over from pact

---
+++ pact-core/Pact/Core/Principal.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Pact.Core.Principal where
+
+import Data.Text(Text)
+
+import Pact.Core.Guards
+import Pact.Core.Names
+
+data Principal
+  = K !PublicKeyText
+    -- ^ format: `k:public key`, where hex public key
+    -- is the text public key of the underlying keyset
+  | W !Text !Text
+    -- ^ format: `w:b64url-encoded hash:pred` where
+    -- the hash is a b64url-encoding of the hash of
+    -- the list of public keys of the multisig keyset
+  | R !KeySetName
+    -- ^ format: `r:keyset-name` where keyset name is
+    -- any definable keyset name
+  | U !Text !Text
+    -- ^ format: `u:fqn of user guard function:b64url-encoded
+    -- hash of args
+  | M !ModuleName !Text
+    -- ^ format: `m:fq module name:fqn of module guard function
+  | P !PactId !Text
+    -- ^ format: `p:pactid:fqn of pact function
+  | C !Text
+    -- ^ format: `c:hash of cap name + cap params + pactId if any
+  deriving (Eq, Ord, Show)
+
+-- | Given a principal type, construct its textual representation
+--
+-- Invariant: should roundtrip with parser.
+--
+mkPrincipalIdent :: Principal -> Text
+mkPrincipalIdent = undefined {- \case
+  P pid n -> "p:" <> asString pid <> ":" <> n
+  K pk -> "k:" <> asString pk
+  W ph n -> "w:" <> ph <> ":" <> n
+  R n -> "r:" <> asString n
+  U n ph -> "u:" <> n <> ":" <> ph
+  M mn n -> "m:" <> asString mn <> ":" <> n
+  C c -> "c:" <> c -}
