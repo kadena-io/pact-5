93d87951e80baced6760df9d15f0f90d068810ad
jmcardon <cardona.josem@gmail.com>
2023-10-20 14:10:03 -0400

    missing files

---
+++ pact-core/Pact/Core/Environment/Types.hs
@@ -0,0 +1,180 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE InstanceSigs #-}
+
+module Pact.Core.Environment.Types
+ ( EvalEnv(..)
+ , eeMsgSigs, eePactDb
+ , eeHash, eeMsgBody
+ , eePactStep
+ , eePublicData, eeMode, eeFlags
+ , eeNatives
+ , PactState(..)
+ , psLoaded
+ , TxCreationTime(..)
+ , PublicData(..)
+ , pdPublicMeta, pdBlockHeight
+ , pdBlockTime, pdPrevBlockHash
+ , PublicMeta(..)
+ , pmChainId, pmSender, pmGasLimit
+ , pmGasPrice, pmTTL, pmCreationTime
+ , TTLSeconds(..)
+ , ChainId(..)
+ , cdChainId, cdBlockHeight
+ , cdBlockTime, cdPrevBlockHash
+ , cdSender, cdGasLimit, cdGasPrice
+ , EvalState(..)
+ , HasEvalState(..)
+ , StackFrame(..)
+ , StackFunctionType(..)
+ , flagRep
+ , flagReps
+ , ExecutionFlag(..)
+ , MonadEvalEnv(..)
+ , MonadEvalState(..)
+ , MonadEval
+--  , setEvalState, (%==), useEvalState, usesEvalState
+--  , (.==)
+--  , viewEvalEnv
+--  , viewsEvalEnv
+ ) where
+
+
+import Control.Lens
+import Control.Monad.Except
+import Control.Monad.IO.Class
+import Data.Set(Set)
+import Data.Text(Text)
+import Data.Map.Strict(Map)
+import Data.Default
+
+import qualified Data.Text as T
+import qualified Data.Map.Strict as M
+
+import Pact.Core.Persistence
+import Pact.Core.Capabilities
+import Pact.Core.Guards
+import Pact.Core.PactValue ( PactValue, EnvData )
+import Pact.Core.Hash
+import Pact.Core.Names
+import Pact.Core.Pacts.Types
+import Pact.Core.ChainData
+import Pact.Core.Errors
+import Pact.Core.Gas
+
+-- | Execution flags specify behavior of the runtime environment,
+-- with an orientation towards some alteration of a default behavior.
+-- Thus, a flag should _not_ describe "normal behavior" (the default),
+-- but instead should enable some "unusual" option.
+data ExecutionFlag
+  -- | Disable user module install
+  = FlagDisableModuleInstall
+  -- | Disable database history queries in transactional mode (local-only)
+  | FlagDisableHistoryInTransactionalMode
+  -- | Disable table module guard for read operations in local
+  | FlagAllowReadInLocal
+  -- | Disable emission of pact events
+  | FlagDisablePactEvents
+  -- | FlagEnforceKeyFormats
+  deriving (Eq,Ord,Show,Enum,Bounded)
+
+-- | Flag string representation
+flagRep :: ExecutionFlag -> Text
+flagRep = T.pack . drop 4 . show
+
+-- | Flag string representations
+flagReps :: Map Text ExecutionFlag
+flagReps = M.fromList $ map go [minBound .. maxBound]
+  where go f = (flagRep f,f)
+
+-- From pact
+-- | All of the types included in our evaluation environment.
+data EvalEnv b i
+  = EvalEnv
+  { _eeMsgSigs :: Map PublicKeyText (Set (CapToken QualifiedName PactValue))
+  -- ^ The list of provided keys and scoped capabilities
+  , _eePactDb :: PactDb b i
+  -- ^ The Pact database store
+  , _eeMsgBody :: EnvData PactValue
+  -- ^ Transaction-provided data
+  , _eeHash :: Hash
+  -- ^ The transaction hash
+  , _eePublicData :: PublicData
+  -- ^ Chain data provided to pact
+  , _eePactStep :: Maybe PactStep
+  -- The (possible) defpact execution step
+  , _eeMode :: ExecutionMode
+  -- ^ The pact execution mode: local or transactional
+  , _eeFlags :: Set ExecutionFlag
+  -- ^ The present runtime flags in the eval environment
+  , _eeNatives :: Map Text b
+  }
+
+makeLenses ''EvalEnv
+
+newtype PactState b i
+  = PactState
+  { _psLoaded :: Loaded b i
+  }
+
+makeLenses ''PactState
+
+data StackFunctionType
+  = SFDefun
+  | SFDefcap
+  | SFDefPact
+  deriving (Eq, Show, Enum, Bounded)
+
+data StackFrame
+  = StackFrame
+  { _sfFunction :: Text
+  , _sfModule :: ModuleName
+  , _sfFnType :: StackFunctionType }
+  deriving Show
+
+data EvalState b i
+  = EvalState
+  { _esCaps :: CapState QualifiedName PactValue
+  , _esStack :: [StackFrame]
+  , _esEvents :: [PactEvent PactValue]
+  , _esLoaded :: Loaded b i
+  , _esPactExec :: Maybe PactExec
+  } deriving Show
+
+instance Default (EvalState b i) where
+  def = EvalState def [] [] mempty Nothing
+
+makeClassy ''EvalState
+
+instance HasLoaded (EvalState b i) b i where
+  loaded = esLoaded
+
+class (Monad m) => MonadEvalEnv b i m | m -> b, m -> i where
+  readEnv :: m (EvalEnv b i)
+
+-- | Our monad mirroring `EvalState` for our evaluation state
+class Monad m => MonadEvalState b i m | m -> b, m -> i where
+  getEvalState :: m (EvalState b i)
+  putEvalState :: EvalState b i -> m ()
+  modifyEvalState :: (EvalState b i -> EvalState b i) -> m ()
+
+-- Our General constraint for evaluation and general analysis
+type MonadEval b i m =
+  ( MonadEvalEnv b i m
+  , MonadEvalState b i m
+  , MonadGas m
+  , MonadError (PactError i) m
+  , MonadIO m
+  , Default i
+  , Show i
+  , Show b)
---
+++ pact-core/Pact/Core/Environment/Utils.hs
@@ -0,0 +1,115 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE InstanceSigs #-}
+
+module Pact.Core.Environment.Utils
+ ( setEvalState
+ , (%==), useEvalState, usesEvalState
+ , (.==)
+ , viewEvalEnv
+ , viewsEvalEnv
+ , getModuleData
+ , getModule
+ , throwExecutionError
+ , throwExecutionError'
+ , toFqDep
+ ) where
+
+import Control.Lens
+import Control.Monad.Except
+import Data.Default
+import Data.Maybe(mapMaybe)
+import qualified Data.Map.Strict as M
+
+import Pact.Core.Names
+import Pact.Core.Persistence
+import Pact.Core.IR.Term
+import Pact.Core.Errors
+import Pact.Core.Environment.Types
+import Pact.Core.Hash
+
+viewEvalEnv :: (MonadEvalEnv b i m) => Lens' (EvalEnv b i) s -> m s
+viewEvalEnv l = view l <$> readEnv
+
+viewsEvalEnv :: (MonadEvalEnv b i m) => Lens' (EvalEnv b i) s -> (s -> a) -> m a
+viewsEvalEnv f l = views f l <$> readEnv
+
+setEvalState :: (MonadEvalState b i m) => Traversal' (EvalState b i) s -> s -> m ()
+setEvalState l s = modifyEvalState (set l s)
+
+(.==) :: (MonadEvalState b i m) => Traversal' (EvalState b i) s -> s -> m ()
+l .== s = modifyEvalState (set l s)
+
+-- overEvalState :: (MonadEval b i m) => Lens' (EvalState b i) s -> (s -> s) -> m ()
+-- overEvalState l f = modifyCEKState (over l f)
+
+(%==) :: (MonadEvalState b i m) => Traversal' (EvalState b i) s -> (s -> s) -> m ()
+l %== f = modifyEvalState (over l f)
+
+infix 4 %==, .==
+
+useEvalState :: (MonadEvalState b i m) => Lens' (EvalState b i) s -> m s
+useEvalState l = view l <$> getEvalState
+
+usesEvalState :: (MonadEvalState b i m) => Lens' (EvalState b i) s -> (s -> s') -> m s'
+usesEvalState l f = views l f <$> getEvalState
+
+toFqDep :: ModuleName -> ModuleHash -> Def name t b i -> (FullyQualifiedName, Def name t b i)
+toFqDep modName mhash defn =
+  let fqn = FullyQualifiedName modName (defName defn) mhash
+  in (fqn, defn)
+
+throwExecutionError :: (MonadEval b i m) => i -> EvalError -> m a
+throwExecutionError i e = throwError (PEExecutionError e i)
+
+throwExecutionError' :: (MonadEval b i m) => EvalError -> m a
+throwExecutionError' = throwExecutionError def
+
+-- | getModuleData, but only for modules, no interfaces
+getModule :: (MonadEval b i m) => i -> PactDb b i -> ModuleName -> m (EvalModule b i)
+getModule info pdb mn =
+ useEvalState (esLoaded . loModules . at mn) >>= \case
+   Just (ModuleData md _) -> pure md
+   Just (InterfaceData _ _) ->
+    throwExecutionError info (ExpectedModule mn)
+   Nothing -> do
+    liftDbFunction info (_pdbRead pdb DModules mn) >>= \case
+      Just mdata@(ModuleData md deps) -> do
+        let newLoaded = M.fromList $ toFqDep mn (_mHash md) <$> (_mDefs md)
+        (esLoaded . loAllLoaded) %== M.union newLoaded . M.union deps
+        (esLoaded . loModules) %== M.insert mn mdata
+        pure md
+      Just (InterfaceData _ _) ->
+        throwExecutionError info (ExpectedModule mn)
+      Nothing ->
+        throwExecutionError info (ModuleDoesNotExist mn)
+
+-- | Get or load a module or interface based on the module name
+getModuleData :: (MonadEval b i m) => i -> PactDb b i -> ModuleName -> m (ModuleData b i)
+getModuleData info pdb mn =
+ useEvalState (esLoaded . loModules . at mn) >>= \case
+   Just md -> pure md
+   Nothing -> do
+    liftDbFunction info (_pdbRead pdb DModules mn) >>= \case
+      Just mdata@(ModuleData md deps) -> do
+        let newLoaded = M.fromList $ toFqDep mn (_mHash md) <$> (_mDefs md)
+        (esLoaded . loAllLoaded) %== M.union newLoaded . M.union deps
+        (esLoaded . loModules) %== M.insert mn mdata
+        pure mdata
+      Just ifdata@(InterfaceData iface deps) -> do
+        let mdefs = mapMaybe ifDefToDef (_ifDefns iface)
+        let newLoaded = M.fromList $ toFqDep mn (_ifHash iface) <$> mdefs
+        (esLoaded . loAllLoaded) %== M.union newLoaded . M.union deps
+        (esLoaded . loModules) %== M.insert mn ifdata
+        pure ifdata
+      Nothing ->
+        throwExecutionError info (ModuleDoesNotExist mn)
