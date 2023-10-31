9b6834584058369e8565a822d09b3f1cd95022b2
rsoeldner <r.soeldner@gmail.com>
2023-10-31 10:03:53 +0100

    whups

--- pact-core/Pact/Core/IR/Term.hs
+++ pact-core/Pact/Core/IR/Term.hs
@@ -115,7 +115,7 @@ instance Show (TableSchema name) where
   show (DesugaredTable t) = "DesugardTable(" <> show t <> ")"
   show (ResolvedTable t) = "ResolvedTable(" <> show t <> ")"
 
-data DefTablename info
+data DefTable name info
   = DefTable
   { _dtName :: Text
   , _dtSchema :: TableSchema name
