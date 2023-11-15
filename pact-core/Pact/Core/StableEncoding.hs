d8c573e902d850e58a0add3a83912adebeca9a78
0xd34df00d <0xd34df00d@gmail.com>
2023-11-15 15:27:56 -0600

    Update pact-core/Pact/Core/StableEncoding.hs
    Co-authored-by: rsoeldner <r.soeldner@gmail.com>

--- pact-core/Pact/Core/StableEncoding.hs
+++ pact-core/Pact/Core/StableEncoding.hs
@@ -108,7 +108,7 @@ instance J.Encode (StableEncoding DefPactGuard) where
     [ "pactId" J..= StableEncoding dpid
     , "name" J..= name
     ]
-
+{-# INLINABLE build #-}
 -- | Stable encoding of `UserGuard FullyQualifiedName PactValue`
 instance J.Encode (StableEncoding (UserGuard FullyQualifiedName PactValue)) where
   build (StableEncoding (UserGuard fun args)) = J.object
