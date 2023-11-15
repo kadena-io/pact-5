2c39519b887429a1aebe647d9d42e99d6d4bc23c
rsoeldner <r.soeldner@gmail.com>
2023-11-15 16:52:55 +0100

    wip

--- pact-core/Pact/Core/Serialise/LegacyPact.hs
+++ pact-core/Pact/Core/Serialise/LegacyPact.hs
@@ -1,11 +1,11 @@
 -- | 
 
 module Pact.Core.Serialise.LegacyPact
-  ( encodeModuleData, decodeModuleData
-  , encodeKeySet, decodeKeySet
-  , encodeDefPactExec, decodeDefPactExec
-  , encodeNamespace, decodeNamespace
-  , encodeRowData, decodeRowData
+  ( decodeModuleData
+  , decodeKeySet
+  , decodeDefPactExec
+  , decodeNamespace
+  , decodeRowData
   ) where
 
 import Pact.Core.Names
@@ -17,62 +17,20 @@ import Pact.Core.Namespace
 import Pact.Core.PactValue
 import Data.ByteString (ByteString)
 
-import qualified Pact.JSON.Encode as JE
 import qualified Pact.JSON.Decode as JD
-
-encodeModuleData :: ModuleData RawBuiltin () -> ByteString
-encodeModuleData = undefined
 
 decodeModuleData :: ByteString -> Maybe (ModuleData RawBuiltin ())
 decodeModuleData = undefined
 
 
-encodeKeySet :: KeySet FullyQualifiedName -> ByteString
-encodeKeySet = JE.encodeStrict
-
 decodeKeySet :: ByteString -> Maybe (KeySet FullyQualifiedName)
 decodeKeySet = undefined
-
-
-encodeDefPactExec :: Maybe DefPactExec -> ByteString
-encodeDefPactExec =  undefined
 
 decodeDefPactExec :: ByteString -> Maybe (Maybe DefPactExec)
 decodeDefPactExec = undefined
 
-encodeNamespace :: Namespace -> ByteString
-encodeNamespace = JE.encodeStrict
-
 decodeNamespace :: ByteString -> Maybe Namespace
 decodeNamespace = undefined
 
-encodeRowData :: RowData -> ByteString
-encodeRowData = undefined
-
 decodeRowData :: ByteString -> Maybe RowData
 decodeRowData = undefined
-
-
-
-instance JE.Encode (KeySet FullyQualifiedName) where
-  build (KeySet ks pred) = JE.object
---    [ "pred" JE..= undefined
-    [
-     "keys" JE..= JE.Array ks
-    ]
-
-instance JE.Encode PublicKeyText where
-  build (PublicKeyText k) = JE.build k
-
-instance JE.Encode Namespace where
-  build (Namespace n user admin) = JE.object
-    [ "admin" JE..= admin
-    , "user" JE..= user
-    , "name" JE..= n
-    ]
-
-instance JE.Encode NamespaceName where
-  build = undefined
-
-instance JE.Encode (Guard FullyQualifiedName PactValue) where
-  build = undefined
