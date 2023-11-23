0972be3e26ae544b9cd271caaf914e47668b673c
rsoeldner <r.soeldner@gmail.com>
2023-11-23 20:13:59 +0100

    whupo

---
+++ pact-core-tests/Pact/Core/Test/LegacySerialiseTests.hs
@@ -0,0 +1,23 @@
+-- | 
+{-# LANGUAGE OverloadedStrings #-}
+
+module Pact.Core.Test.LegacySerialiseTests where
+
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.HUnit (assertBool, testCase)
+import Data.Maybe
+import Pact.Core.Serialise.LegacyPact
+import Pact.Core.Guards
+import Data.Text (Text)
+import qualified Data.Text.IO as T
+
+tests :: TestTree
+tests = testGroup "Legacy Serialisation"
+  [ testGroup "KeySet"
+    [ testCase "pred: keys-2"   $ assertBool "KeySet decoding failed" (isJust (decodeKeySet "{\"pred\":\"keys-2\",\"keys\":[\"ddd8\",\"ed0\"]}"))
+    , testCase "pred: keys-all" $ assertBool "KeySet decoding failed" (isJust (decodeKeySet "{\"pred\":\"keys-all\",\"keys\":[\"ddd8\",\"ed0\"]}"))
+    , testCase "pred: keys-any" $ assertBool "KeySet decoding failed" (isJust (decodeKeySet "{\"pred\":\"keys-any\",\"keys\":[\"ddd8\",\"ed0\"]}"))
+    , testCase "pred defaults"  $ assertBool "KeySet decoding failed" (maybe False (\k -> KeysAll == _ksPredFun k) (decodeKeySet "{\"pred\":\"keys-all\",\"keys\":[\"ddd8\",\"ed0\"]}"))
+    , testCase "pred invalid" $ assertBool "Accept invalid pred" (isNothing (decodeKeySet "{\"pred\":\"ABC\",\"keys\":[\"ddd8\",\"ed0\"]}"))
+    ]
+  ]
