0e4fbb9ec5dba4391b4cca5b2937c60915cbbdb5
0xd34df00d <0xd34df00d@gmail.com>
2023-12-01 00:45:58 -0600

    Add missing cases for DefPactGuard

--- pact-core/Pact/Core/IR/ModuleHashing.hs
+++ pact-core/Pact/Core/IR/ModuleHashing.hs
@@ -110,6 +110,7 @@ updateGuardHash mname mhash = \case
         (updatePactValueHash mname mhash <$> pvs)
         pid
   GModuleGuard mg -> GModuleGuard mg
+  g@GDefPactGuard{} -> g
 
 updatePactValueHash :: ModuleName -> ModuleHash -> PactValue -> PactValue
 updatePactValueHash mname mhash = \case
@@ -225,6 +226,7 @@ encodeGuard = \case
     "CapGuard" <> encodeApp (encodeFqnAsQual ct) (encodePactValue <$> args)
   GModuleGuard (ModuleGuard mg n) ->
     "ModuleGuard" <> parens (encodeModuleName mg <+> encodeText n)
+  GDefPactGuard (DefPactGuard (DefPactId dpid) name) -> "DefPactGuard" <> parens (encodeText dpid <+> encodeText name)
 
 encodeModuleName :: ModuleName -> Builder
 encodeModuleName = encodeText . renderModuleName
