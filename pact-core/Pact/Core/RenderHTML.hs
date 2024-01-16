988d184278e6ce08955dfbd4f6a522860405d0f8
Marcin Grzybowski <marcinjangrzybowski@gmail.com>
2024-01-16 13:20:34 +0100

    wip

--- pact-core/Pact/Core/RenderHTML.hs
+++ pact-core/Pact/Core/RenderHTML.hs
@@ -11,8 +11,9 @@ import Pact.Core.Compile
 import Pact.Core.Syntax.Node.Node
 import Pact.Core.Syntax.ParseTreeNode
 
+-- import import System.File.Tree (getDirectory, copyTo_)
 
-
+import Control.Monad
 import System.Environment (getArgs)
 import System.FilePath
 import System.Directory 
@@ -21,31 +22,31 @@ import System.Directory
   , createDirectoryIfMissing
   , listDirectory
   , copyFile)
-
+import Path.Posix (parseAbsDir)
+import Path.IO (copyDirRecur)
 
 
 main :: IO ()
-main = do
-  (target:output:_) <- getArgs
-  isFile <- doesFileExist target
-  isDir <- doesDirectoryExist target
-  if isFile then processFile target output
-  else if isDir then processDir target output
-  else putStrLn "The given target is neither a file nor a directory."
-
+main = 
+  getArgs >>= \case
+    (target:output:_) -> do
+       if (takeExtension target ==".pact")
+       then processFile target output
+       else processDir target output
+      
+    _ -> putStrLn "TODO: usage info"
 
 folderWithAssets :: FilePath
 folderWithAssets = "/Users/marcin/www-pact"
 
 
 assetsToCopy :: [FilePath]
-assetsToCopy = ["main.js", "style.css", "webfonts"]
+assetsToCopy = ["main.js", "style.css", "webfonts","dat"]
 
 
 runRenderHTML :: FilePath -> IO B.ByteString
 runRenderHTML file = do
 
-  -- let src = "/Users/marcin/pact-projects/marmalade/pact/root/coin.pact"
   r <- _parseOnlyFile file 
   case r of
     Left e -> return "todo: errorhandling" --putStr (show e)
@@ -63,27 +64,48 @@ processFile file output = do
   B.writeFile targetFile content
   copyAssets folderWithAssets output
 
+
 processDir :: FilePath -> FilePath -> IO ()
 processDir root output = do
-  files <- listDirectory root
-  let pactFiles = filter ((==".pact") . takeExtension) files
-  mapM_ (processPactFile root output) pactFiles
-  copyAssets folderWithAssets output
+  createDirectoryIfMissing True output
+  visitPath root output
+
+visitPath :: FilePath -> FilePath -> IO ()
+visitPath root output = do
+  entries <- listDirectory root
+  forM_ entries $ \entry -> do
+    let fullPath = root </> entry
+    isDir <- doesDirectoryExist fullPath
+    isFile <- doesFileExist fullPath
+    when isDir $ visitPath fullPath (output </> entry)
+    when (isFile && takeExtension entry == ".pact") $ do
+      processPactFile root output fullPath
 
 processPactFile :: FilePath -> FilePath -> FilePath -> IO ()
 processPactFile root output pactFile = do
   let relativePath = makeRelative root pactFile
-  let targetHtmlFile = replaceExtension relativePath ".html"
+  let targetHtmlFile = replaceExtension relativePath "html" -- Corrected extension
   let outputDir = takeDirectory $ output </> targetHtmlFile
   createDirectoryIfMissing True outputDir
-  content <- runRenderHTML pactFile
+  content <- runRenderHTML (pactFile)
   B.writeFile (output </> targetHtmlFile) content
-  copyAssets folderWithAssets outputDir
+  copyAssets folderWithAssets outputDir -- Copy assets into the directory of the HTML file
 
 copyAssets :: FilePath -> FilePath -> IO ()
 copyAssets source dest
   | source == dest = return ()
   | otherwise = mapM_ (copyAsset source dest) assetsToCopy
 
+
 copyAsset :: FilePath -> FilePath -> FilePath -> IO ()
-copyAsset source dest asset = copyFile (source </> asset) (dest </> asset)
+copyAsset source dest asset = do
+  let sourcePath = source </> asset
+  let destPath = dest </> asset
+  isDir <- doesDirectoryExist sourcePath
+  if isDir
+    then do
+     s <- parseAbsDir sourcePath
+     d <- parseAbsDir destPath
+     copyDirRecur s d 
+    else do
+      copyFile (source </> asset) (dest </> asset)
