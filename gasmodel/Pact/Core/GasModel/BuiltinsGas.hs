0e320f783d4d5136649d9671bac65ed87cb7ef41
0xd34df00d <0xd34df00d@gmail.com>
2024-03-04 14:38:57 -0600

    Cleanup imports

--- gasmodel/Pact/Core/GasModel/BuiltinsGas.hs
+++ gasmodel/Pact/Core/GasModel/BuiltinsGas.hs
@@ -3,39 +3,15 @@
 
 module Pact.Core.GasModel.BuiltinsGas where
 
-import Control.Lens
--- import Control.Monad
-import Control.Monad.IO.Class
-import Data.Default
--- import Data.Functor(void)
--- import Data.Bifunctor(bimap)
--- import Criterion.Types(Report)
-import qualified Data.RAList as RA
-import qualified Data.List.NonEmpty as NE
 import qualified Criterion as C
--- import qualified Criterion.Report as C
--- import qualified Criterion.Analysis as C
 import qualified Data.Text as T
-import qualified Data.Set as S
-import qualified Data.Map.Strict as M
 import qualified Database.SQLite3 as SQL
 import NeatInterpolation (text)
 
 import Pact.Core.Builtin
-import Pact.Core.Environment
-import Pact.Core.Names
-import Pact.Core.Gas
-import Pact.Core.Literal
-import Pact.Core.Type
-import Pact.Core.Capabilities
-import Pact.Core.IR.Eval.Runtime
-import Pact.Core.PactValue
-import Pact.Core.IR.Term
 import Pact.Core.Persistence
-import Pact.Core.Hash
 import Pact.Core.Persistence.SQLite
 import Pact.Core.Serialise (serialisePact)
-import qualified Pact.Core.IR.Eval.CEK as Eval
 
 import Pact.Core.GasModel.Utils
 
