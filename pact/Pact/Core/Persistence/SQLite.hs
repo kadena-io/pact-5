d84477a11c9f85a2c17f95b1f6880eece229d9d4
rsoeldner <r.soeldner@gmail.com>
2024-06-16 21:48:55 +0200

    fix stmt caching

--- pact/Pact/Core/Persistence/SQLite.hs
+++ pact/Pact/Core/Persistence/SQLite.hs
@@ -7,10 +7,10 @@
 
 module Pact.Core.Persistence.SQLite (
   withSqlitePactDb,
---   unsafeCreateSqlitePactDb
+  unsafeCreateSqlitePactDb
  ) where
 
--- import Control.Monad --
+import Control.Monad
 import Control.Monad.Catch
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.IORef
@@ -32,6 +32,7 @@ import Pact.Core.Literal
 import Control.Exception (throwIO)
 import Pact.Core.Serialise
 
+import GHC.Stack
 -- | Acquire a SQLite-backed `PactDB`.
 --
 -- Pact entities (modules, keysets, namespaces, defpacts, and usertables) will be stored
@@ -40,7 +41,7 @@ import Pact.Core.Serialise
 -- and how they should be serialized. [`serializePact`] is a good default.
 --
 withSqlitePactDb
-  :: (MonadMask m, MonadIO m)
+  :: (MonadMask m, MonadIO m, HasCallStack)
   => PactSerialise b i
   -> Text
   -> (PactDb b i -> m a)
@@ -48,46 +49,51 @@ withSqlitePactDb
 withSqlitePactDb serial connectionString act =
   bracket connect cleanup work
   where
-    work db = do
-      (pdb, cache) <- liftIO $ initializePactDb serial db
-      r <- act pdb
-      liftIO $ do
-        cache' <- readIORef cache
-        _ <- finalizeUserTable (_stmtUserTbl cache')
-        finalizeStmt (_stmtNamespace cache')
-        finalizeStmt (_stmtKeyset cache')
-        finalizeStmt (_stmtModules cache')
-        finalizeStmt (_stmtDefPact cache')
-      pure r
-    connect = liftIO $ SQL.open connectionString
-    cleanup db = liftIO $ SQL.close db
+    work (_,(pdb, _)) = act pdb
+    connect = liftIO $ do
+      con <- SQL.open connectionString
+      forM_ fastNoJournalPragmas $ \p -> liftIO (SQL.exec con ("PRAGMA " <> p))
+
+      (con,) <$> initializePactDb serial con
+      
+    cleanup (db, (_pdb, cache)) = liftIO $ do
+      cache' <- readIORef cache
+      _ <- finalizeUserTable (_stmtUserTbl cache')
+      finalizeStmt (_stmtNamespace cache')
+      finalizeStmt (_stmtKeyset cache')
+      finalizeStmt (_stmtModules cache')
+      finalizeStmt (_stmtDefPact cache')
+      SQL.close db
+
     finalizeUserTable  = traverse finalizeStmt
-    finalizeStmt (TblStatements i u rv rk) = do
-      SQL.finalize i
-      SQL.finalize u
-      SQL.finalize rv
-      SQL.finalize rk
+
+finalizeStmt :: TblStatements -> IO ()
+finalizeStmt (TblStatements i u rv rk) = do
+    SQL.finalize i
+    SQL.finalize u
+    SQL.finalize rv
+    SQL.finalize rk
 
 -- | Acquire the sqlite pact db, but do not close the connection
 -- NOTE: This functions is exposed for benchmarking, but should _not_ be used
 -- anywhere else in the runtime unless otherwise needed
--- unsafeCreateSqlitePactDb
---   :: (MonadIO m)
---   => PactSerialise b i
---   -> Text
---   -> m (PactDb b i, SQL.Database)
--- unsafeCreateSqlitePactDb serial connectionString  = do
---   db <- liftIO $ SQL.open connectionString
---   _ <- forM_ fastNoJournalPragmas $ \p -> liftIO (SQL.exec db ("PRAGMA " <> p))
---   (,db) <$> liftIO (initializePactDb serial db)
-
--- fastNoJournalPragmas :: [Text]
--- fastNoJournalPragmas = [
---   "synchronous = OFF",
---   "journal_mode = MEMORY",
---   "locking_mode = EXCLUSIVE",
---   "temp_store = MEMORY"
---   ]
+unsafeCreateSqlitePactDb
+  :: (MonadIO m)
+  => PactSerialise b i
+  -> Text
+  -> m (PactDb b i, SQL.Database)
+unsafeCreateSqlitePactDb serial connectionString = liftIO $ do
+  con <- SQL.open connectionString
+  (pdb, _) <- initializePactDb serial con
+  pure (pdb, con)
+      
+fastNoJournalPragmas :: [Text]
+fastNoJournalPragmas = [
+  "synchronous = OFF",
+  "journal_mode = MEMORY",
+  "locking_mode = EXCLUSIVE",
+  "temp_store = MEMORY"
+  ]
 
 createSysTables :: SQL.Database -> IO StmtCache
 createSysTables db = do
@@ -126,7 +132,7 @@ mkTblStatement db tbl = do
       insertStmt <- SQL.prepare db ("INSERT INTO \"" <> tbl <> "\" (txid, rowkey, rowdata) VALUES (?,?,?)")
       insertOrUpdateStmt <- SQL.prepare db ("INSERT OR REPLACE INTO \"" <> tbl <> "\" (txid, rowkey, rowdata) VALUES (?,?,?)")
       readValueStmt <- SQL.prepare db ("SELECT rowdata FROM \""<> tbl <> "\" WHERE rowkey = ? ORDER BY txid DESC LIMIT 1")
-      readKeysStmt <-  SQL.prepare db ("SELECT rowkey FROM \""<> tbl <> "\" WHERE rowkey = ? ORDER BY txid")
+      readKeysStmt <-  SQL.prepare db ("SELECT rowkey FROM \""<> tbl <> "\" ORDER BY txid DESC")
       pure $ TblStatements insertStmt insertOrUpdateStmt readValueStmt readKeysStmt
 
 
@@ -171,17 +177,17 @@ getTxLog serial db currTxId txLog tab txId = do
     case env of
       Nothing -> fail "undexpected decoding error"
       Just xs -> pure xs
-    else do
-    withStmt (SQL.prepare db $ "SELECT rowkey,rowdata FROM \"" <> toUserTable tab <> "\" WHERE txid = ?") $ \stmt -> do
-      let TxId i = txId
-      SQL.bind stmt [SQL.SQLInteger $ fromIntegral i]
-      txLogBS <- collect stmt []
-      case traverse (traverse (fmap (view document) . _decodeRowData serial)) txLogBS of
-        Nothing -> fail "unexpected decoding error"
-        Just txl -> pure txl
-  where
+    else withStmtClear (SQL.prepare db $ "SELECT rowkey,rowdata FROM \"" <> toUserTable tab <> "\" WHERE txid = ?") $ \stmt -> do
+       let TxId i = txId
+       SQL.clearBindings stmt
+       SQL.bind stmt [SQL.SQLInteger $ fromIntegral i]
+       txLogBS <- collect stmt []
+       case traverse (traverse (fmap (view document) . _decodeRowData serial)) txLogBS of
+         Nothing -> fail "unexpected decoding error"
+         Just txl -> pure txl
+   where
     collect stmt acc = SQL.step stmt >>= \case
-        SQL.Done ->  pure acc
+        SQL.Done -> SQL.reset stmt >> pure acc
         SQL.Row -> do
           [SQL.SQLText key, SQL.SQLBlob value] <- SQL.columns stmt
           collect stmt (TxLog (toUserTable tab) key value:acc)
@@ -195,36 +201,36 @@ readKeys _db stmtCache = \case
       Right v -> pure v
 
   DModules -> withStmt (_tblReadKeys . _stmtModules <$> readIORef stmtCache) $ \stmt ->
-    fmap parseModuleName <$> collect stmt [] >>= \mns -> case sequence mns of
+     fmap parseModuleName <$> collect stmt [] >>= \mns -> case sequence mns of
       Nothing -> fail "unexpected decoding"
       Just mns' -> pure mns'
 
   DDefPacts -> withStmt (_tblReadKeys . _stmtDefPact <$> readIORef stmtCache) $ \stmt ->
-    fmap DefPactId <$> collect stmt []
+     fmap DefPactId <$> collect stmt []
   
   DNamespaces -> withStmt (_tblReadKeys . _stmtNamespace <$> readIORef stmtCache) $ \stmt ->
-    fmap NamespaceName <$> collect stmt []
+     fmap NamespaceName <$> collect stmt []
+
 
   DUserTables tbl -> do
-    tblCache <- _stmtUserTbl <$> readIORef stmtCache
-    case Map.lookup tbl tblCache of
-      Nothing -> fail "invariant failure: table unknown"
-      Just stmt -> withStmt (pure $ _tblReadKeys stmt) $ \s -> fmap RowKey <$> collect s []
+     tblCache <- _stmtUserTbl <$> readIORef stmtCache
+     case Map.lookup tbl tblCache of
+       Nothing -> fail "invariant failure: table unknown"
+       Just stmt -> withStmt (pure $ _tblReadKeys stmt) $ \s -> fmap RowKey <$> collect s []
   where
     collect stmt acc = SQL.step stmt >>= \case
-        SQL.Done -> pure acc
-        SQL.Row -> do
+       SQL.Done -> SQL.reset stmt >> pure acc
+       SQL.Row -> do
           [SQL.SQLText value] <- SQL.columns stmt
           collect stmt (value:acc)
 
-
 listTxIds :: SQL.Database -> TableName -> TxId -> IO [TxId]
-listTxIds db tbl (TxId minTxId) = withStmt (SQL.prepare db $ "SELECT txid FROM \"" <> toUserTable tbl <> "\" WHERE txid >= ? ORDER BY txid ASC") $ \stmt -> do
-  SQL.bind stmt [SQL.SQLInteger $ fromIntegral minTxId]
-  collect stmt []
+listTxIds db tbl (TxId minTxId) = withStmtClear (SQL.prepare db $ "SELECT txid FROM \"" <> toUserTable tbl <> "\" WHERE txid >= ? ORDER BY txid ASC") $ \stmt -> do
+    SQL.bind stmt [SQL.SQLInteger $ fromIntegral minTxId]
+    collect stmt []
   where
     collect stmt acc = SQL.step stmt >>= \case
-        SQL.Done -> pure acc
+        SQL.Done -> SQL.reset stmt >>pure acc
         SQL.Row -> do
           [SQL.SQLInteger value] <- SQL.columns stmt
           -- Here we convert the Int64 received from SQLite into Word64
@@ -270,15 +276,20 @@ createUserTable serial db txLog stmtCache tbl = do
     SQL.exec db stmt
     modifyIORef' txLog (TxLog "SYS:usertables" (_tableName tbl) rdEnc :)
     stmts <- mkTblStatement db tblName
-    modifyIORef' stmtCache (\c -> c{_stmtUserTbl = Map.insert tbl stmts (_stmtUserTbl c)})
-
+    cache <- readIORef stmtCache
+    let insert = modifyIORef' stmtCache (\c -> c{_stmtUserTbl = Map.insert tbl stmts (_stmtUserTbl c)})
+    case Map.lookup tbl (_stmtUserTbl cache) of
+      Nothing -> insert
+      Just old -> do
+        finalizeStmt old
+        insert
   where
-    stmt = "CREATE TABLE IF NOT EXISTS " <> tblName <> " \
+    stmt = "CREATE TABLE IF NOT EXISTS \"" <> tblName <> "\" \
            \ (txid UNSIGNED BIG INT, \
            \  rowkey TEXT, \
            \  rowdata BLOB, \
            \  UNIQUE (txid, rowkey))"
-    tblName = "\"" <> toUserTable tbl <> "\""
+    tblName = toUserTable tbl
 
 write'
   :: forall k v b i
@@ -326,29 +337,30 @@ write' serial db txId txLog stmtCache wt domain k v =
     DKeySets -> liftIO $ withStmt (_tblInsertOrUpdate . _stmtKeyset <$> readIORef stmtCache) $ \stmt -> do
       let encoded = _encodeKeySet serial v
       TxId i <- readIORef txId
+      SQL.clearBindings stmt
       SQL.bind stmt [SQL.SQLInteger (fromIntegral i), SQL.SQLText (renderKeySetName k), SQL.SQLBlob encoded]
       doWrite stmt (TxLog "SYS:KEYSETS" (renderKeySetName k) encoded:)
-
+      
     DModules -> liftIO $ withStmt (_tblInsertOrUpdate . _stmtModules <$> readIORef stmtCache) $ \stmt -> do
       let encoded = _encodeModuleData serial v
       TxId i <- readIORef txId
       SQL.bind stmt [SQL.SQLInteger (fromIntegral i), SQL.SQLText (renderModuleName k), SQL.SQLBlob encoded]
       doWrite stmt (TxLog "SYS:MODULES" (renderModuleName k) encoded:)
-
+      
     DDefPacts -> liftIO $ withStmt (_tblInsertOrUpdate . _stmtDefPact <$> readIORef stmtCache) $ \stmt -> do
-      let encoded = _encodeDefPactExec serial v
-          DefPactId k' = k
-      TxId i <- readIORef txId
-      SQL.bind stmt [SQL.SQLInteger (fromIntegral i), SQL.SQLText k', SQL.SQLBlob encoded]
-      doWrite stmt (TxLog "SYS:PACTS" k' encoded:)
+       let encoded = _encodeDefPactExec serial v
+           DefPactId k' = k
+       TxId i <- readIORef txId
+       SQL.bind stmt [SQL.SQLInteger (fromIntegral i), SQL.SQLText k', SQL.SQLBlob encoded]
+       doWrite stmt (TxLog "SYS:PACTS" k' encoded:)
   
-    DNamespaces -> liftIO $ withStmt (_tblInsertOrUpdate . _stmtNamespace <$> readIORef stmtCache) $ \stmt -> do
+    DNamespaces ->
+      liftIO $ withStmt (_tblInsertOrUpdate . _stmtNamespace <$> readIORef stmtCache) $ \stmt -> do
       let encoded = _encodeNamespace serial v
           NamespaceName k' = k
       TxId i <- readIORef txId
       SQL.bind stmt [SQL.SQLInteger (fromIntegral i), SQL.SQLText k', SQL.SQLBlob encoded]
       doWrite stmt  (TxLog "SYS:NAMESPACES" k' encoded:)
-  
   where
     checkInsertOk ::  TableName -> RowKey -> IO (Maybe RowData)
     checkInsertOk tbl rk = do
@@ -362,9 +374,11 @@ write' serial db txId txLog stmtCache wt domain k v =
         (Nothing, Update) -> throwIO (E.NoRowFound tbl rk)
 
     doWrite stmt txlog = Direct.stepNoCB stmt >>= \case
-          Left _ -> throwIO E.WriteException
+          Left _ ->  throwIO E.WriteException
           Right res
-            | res == SQL.Done -> modifyIORef' txLog txlog
+            | res == SQL.Done -> do
+                SQL.reset stmt
+                modifyIORef' txLog txlog
             | otherwise -> throwIO E.MultipleRowsReturnedFromSingleWrite
 
 read' :: forall k v b i. PactSerialise b i -> SQL.Database -> IORef StmtCache -> Domain k v b i -> k -> IO (Maybe v)
@@ -390,17 +404,22 @@ read' serial _db stmtCache domain k = case domain of
     (doRead (_namespaceName k) (\v -> pure (view document <$> _decodeNamespace serial v)))
 
   where
+    doRead :: forall a. Text -> (ByteString -> IO (Maybe a)) -> SQL.Statement -> IO (Maybe a)
     doRead k' f stmt = do
-      SQL.bind stmt [SQL.SQLText k']
-      SQL.step stmt >>= \case
-        SQL.Done -> pure Nothing
-        SQL.Row -> do
-          1 <- SQL.columnCount stmt
-          [SQL.SQLBlob value] <- SQL.columns stmt
-          SQL.Done <- SQL.step stmt
-          f value
-
+       SQL.bind stmt [SQL.SQLText k']
+       SQL.step stmt >>= \case
+         SQL.Done -> do
+           SQL.reset stmt
+           pure Nothing
+         SQL.Row -> do
+           [SQL.SQLBlob value] <- SQL.columns stmt
+           SQL.Done <- SQL.step stmt
+           SQL.reset stmt
+           f value
 
 -- -- Utility functions
 withStmt :: IO SQL.Statement -> (SQL.Statement -> IO a) -> IO a
-withStmt stmt = bracket stmt SQL.finalize
+withStmt stmt = bracket stmt SQL.clearBindings
+
+withStmtClear :: IO SQL.Statement -> (SQL.Statement -> IO a) -> IO a
+withStmtClear stmt = bracket stmt SQL.finalize
