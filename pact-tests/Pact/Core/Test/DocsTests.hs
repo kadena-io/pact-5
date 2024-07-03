36fba2ce665316d0fecbb92f6e990f32c4b6ae4c
rsoeldner <r.soeldner@gmail.com>
2024-07-03 16:34:55 +0200

    add builtin docs test

---
+++ pact-tests/Pact/Core/Test/DocsTests.hs
@@ -0,0 +1,59 @@
+-- | 
+
+module Pact.Core.Test.DocsTests
+  (tests)
+where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+import System.Directory
+import System.FilePath
+import Paths_pact_tng
+import Control.Monad
+import qualified Data.Set as S
+import Data.Text (Text)
+import qualified Data.Text as T
+import Pact.Core.Builtin
+
+tests :: IO TestTree
+tests = do
+  baseDir <- (</> "docs/builtins") <$> getDataDir
+  cats <- listDirectory baseDir
+  builtinWithDocs <- join <$> traverse (\cat -> map (T.pack . takeBaseName) <$> listDirectory (baseDir </> cat)) cats
+  pure $ testGroup "Documentation Tests"
+    [ docsExistsTest builtinWithDocs
+    ]
+
+docsExistsTest :: [Text] -> TestTree
+docsExistsTest b = testCase "Builtins should have docs" $ do
+  let normBuiltins = normalizeBuiltinName <$> replCoreBuiltinNames
+  let diff = S.fromList normBuiltins `S.difference` S.fromList b
+  assertEqual "Missing builtins should be empty" S.empty diff
+
+
+normalizeBuiltinName :: Text -> Text
+normalizeBuiltinName = \case
+  "!=" -> "neq"
+  "&"  -> "and"
+  "*"  -> "mult"
+  "+"  -> "add"
+  "-"  -> "sub"
+  "/" -> "div"
+  "<" -> "lt"
+  "<=" -> "leq"
+  "="  -> "eq"
+  ">"  -> "gt"
+  ">=" -> "geq"
+  "^" -> "pow"
+  "and?" -> "and-q"
+  "not?" -> "not-q"
+  "or?"  -> "or-q"
+  "|"    -> "bitwise-or"
+  "~"    -> "binary-inverse"
+  "begin-named-tx" -> "begin-tx"
+  "continue-pact-rollback-yield" -> "continue-pact"
+  "continue-pact-rollback-yield-object" -> "continue-pact"
+  "continue-pact-with-rollback" -> "continue-pact"
+  "enforce-pact-version-range" -> "enforce-pact-version"
+  "env-set-gas" -> "env-gas"
+  other  -> other
